143f28a162abb955ccf61352b5c3ebbb2d219aa3
Adrian Meza <adrianme213@gmail.com>
2018-03-22 14:38:14 -0700

    [feat] - Hello World with React component renders

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      games: []
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
+export default App;
